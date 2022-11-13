da64689a03f110cd96ee7920801f24a3a97f5181
JiquanZhong <74232427+JiquanZhong@users.noreply.github.com>
2022-11-13 05:49:34 +0100

    add all

---
+++ ssmbuild/src/main/java/com/jiquan/pojo/Books.java
@@ -0,0 +1,45 @@
+package com.jiquan.pojo;
+
+/**
+ * @author ZHONG Jiquan
+ * @create 13/11/2022 - 05:31
+ */
+
+public class Books {
+    private int bookID;
+    private String bookName;
+    private int bookCounts;
+    private String detail;
+
+    public int getBookID() {
+        return bookID;
+    }
+
+    public void setBookID(int bookID) {
+        this.bookID = bookID;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public int getBookCounts() {
+        return bookCounts;
+    }
+
+    public void setBookCounts(int bookCounts) {
+        this.bookCounts = bookCounts;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+}
